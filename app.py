82504360fd60fd55c726d828dab3f43199164ee7
123khg <62598466+123khg@users.noreply.github.com>
2023-03-11 14:29:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 
 if st.button('Giải'):
-  st.write('Phương trình có 1 nghiệm ' + str(-b/a))
+  if a==0:
+    st.write('Phương trình có vô số nghiệm')
+  else:
+    st.write('Phương trình có 1 nghiệm ' + str(-b/a))
  
